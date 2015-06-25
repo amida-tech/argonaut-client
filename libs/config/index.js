4b83e69f5ec52828e4d28ae4cead3804fe359613
OEHokie <matt.martz@gmail.com>
2015-06-25 17:44:48 -0400

    settings page

--- libs/config/index.js
+++ libs/config/index.js
@@ -1,65 +1,68 @@
 /**
  * Provides pre-configured list of servers and applications registered on them.
  */
- 
+
 var clients = [{
-        name: 'SMART on FHIR',
-        url: 'https://fhir-api.smarthealthit.org',
-        auth_url: 'https://authorize.smarthealthit.org',
-        logo_url: '',
-        credentials: {
-            client_id: '277923e5-5258-4070-bce7-67194bc62523',
-            client_secret: 'do_Cw29XRO18cLjRPYN-svCOPcspjBxXbLlRvfbNn-UNexm_ig68ABt_gxWYvWAkHWjpuCePMndTXgIdLDtsoA',
-            site: 'https://authorize.smarthealthit.org',
-            api_url: 'https://fhir-api.smarthealthit.org',
-            authorization_path: '/authorize',
-            token_path: '/token',
-            revocation_path: '/revoke',
-            scope: '',
-            redirect_uri: 'http://toolbox.amida-demo.com:3001/fhir/callback'
-        }
-    }, {
-        name: 'DRE/FHIR (localhost:3000)',
-        url: 'http://localhost:3000/',
-        auth_url: 'http://localhost:3000/',
-        logo_url: '',
-        credentials: {
-            client_id: 'argonaut_demo_client_local',
-            client_secret: 'have no secrets!',
-            site: 'http://localhost:3000/',
-            api_url: 'http://localhost:3000/fhir',
-            authorization_path: 'oauth2/authorize',
-            token_path: 'oauth2/token',
-            revocation_path: 'oauth2/revoke',
-            scope: '',
-            redirect_uri: 'http://localhost:3001/fhir/callback'
-        }
-    }/*, { //commented out for demo/screenshots
-        name: 'DRE/FHIR (toolbox.amida-demo.com:3000)',
-        url: 'http://toolbox.amida-demo.com:3000/',
-        auth_url: 'http://toolbox.amida-demo.com:3000/',
-        logo_url: '',
-        credentials: {
-            client_id: 'argonaut_demo_client',
-            client_secret: 'have no secrets!',
-            site: 'http://toolbox.amida-demo.com:3000/',
-            api_url: 'http://toolbox.amida-demo.com:3000/fhir',
-            authorization_path: 'oauth2/authorize',
-            token_path: 'oauth2/token',
-            revocation_path: 'oauth2/revoke',
-            scope: '',
-            redirect_uri: 'http://toolbox.amida-demo.com:3001/fhir/callback'
-        }
-    }*/];
-    
+    name: 'DRE/FHIR (localhost:3000)',
+    url: 'http://localhost:3000/',
+    auth_url: 'http://localhost:3000/',
+    logo_url: '',
+    credentials: {
+        client_id: 'argonaut_demo_client_local',
+        client_secret: 'have no secrets!',
+        site: 'http://localhost:3000/',
+        api_url: 'http://localhost:3000/fhir',
+        authorization_path: 'oauth2/authorize',
+        token_path: 'oauth2/token',
+        revocation_path: 'oauth2/revoke',
+        scope: '',
+        redirect_uri: 'http://localhost:3001/fhir/callback'
+    }
+}, {
+    name: 'SMART on FHIR',
+    url: 'https://fhir-api.smarthealthit.org',
+    auth_url: 'https://authorize.smarthealthit.org',
+    logo_url: '',
+    credentials: {
+        client_id: '277923e5-5258-4070-bce7-67194bc62523',
+        client_secret: 'do_Cw29XRO18cLjRPYN-svCOPcspjBxXbLlRvfbNn-UNexm_ig68ABt_gxWYvWAkHWjpuCePMndTXgIdLDtsoA',
+        site: 'https://authorize.smarthealthit.org',
+        api_url: 'https://fhir-api.smarthealthit.org',
+        authorization_path: '/authorize',
+        token_path: '/token',
+        revocation_path: '/revoke',
+        scope: '',
+        redirect_uri: 'http://toolbox.amida-demo.com:3001/fhir/callback'
+    }
+}/*, { //commented out for demo/screenshots
+ name: 'DRE/FHIR (toolbox.amida-demo.com:3000)',
+ url: 'http://toolbox.amida-demo.com:3000/',
+ auth_url: 'http://toolbox.amida-demo.com:3000/',
+ logo_url: '',
+ credentials: {
+ client_id: 'argonaut_demo_client',
+ client_secret: 'have no secrets!',
+ site: 'http://toolbox.amida-demo.com:3000/',
+ api_url: 'http://toolbox.amida-demo.com:3000/fhir',
+ authorization_path: 'oauth2/authorize',
+ token_path: 'oauth2/token',
+ revocation_path: 'oauth2/revoke',
+ scope: '',
+ redirect_uri: 'http://toolbox.amida-demo.com:3001/fhir/callback'
+ }
+ }*/];
+
 module.exports = {
     //fhir_server_uri: 'https://fhir-dev.healthintersections.com.au/closed'
     //fhir_server_uri: 'https://fhir-api.smarthealthit.org',
-    
-    findClient: function(client_id) {
-            var i, len = clients.length;
-    var credentials; 
-    for(i=0; i<len;i++) if(client_id === clients[i].credentials.client_id) { credentials =  clients[i].credentials; break;}
+
+    findClient: function (client_id) {
+        var i, len = clients.length;
+        var credentials;
+        for (i = 0; i < len; i++) if (client_id === clients[i].credentials.client_id) {
+            credentials = clients[i].credentials;
+            break;
+        }
         return credentials;
     },
     clients: clients
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var dal = require('../libs/dal');
 var fhir = require('../libs/fhir');
 var config = require('../libs/config');
 var Promise = require('promise');
+var async = require('async');
 
 /* GET home page. */
 /**
@@ -35,6 +36,91 @@ router.get('/', function (req, res) {
         });
     });
 
+});
+
+router.get('/settings', function (req, res) {
+
+    var readUsers = new Promise(function (resolve, reject) {
+        dal.users.readAll(function (err, users) {
+            if (err) reject(err);
+            else resolve(users);
+        });
+    });
+
+    readUsers.then(function (rawUsers) {
+        var users = {};
+        var newClients = [];
+        var clients = config.clients;
+        async.eachSeries(clients,function(client,cb){
+            client.userExists = false;
+            async.eachSeries(rawUsers, function(user,cb2){
+                if (!client.userExists) {
+                    if (user.clientId === client.credentials.client_id) {
+                        client.user = user;
+                        client.userExists = true;
+                        fhir.request(user.clientId, user.accessToken, '/Patient').then(function(val) {
+                            var json = JSON.parse(val);
+                            client.userInfo = json.entry;
+                            cb2();
+                        }).catch(function(err) {
+                            client.userInfo = {};
+                            cb2();
+                        });
+                    } else {
+                        cb2();
+                    }
+                } else {
+                    cb2();
+                }
+            },function(err) {
+                newClients.push(client);
+                cb();
+            })
+        },function(err) {
+            console.log("new clients: "+JSON.stringify(newClients,null,4));
+            res.render('settings', {
+                title: 'Express',
+                users: users,
+                clients: newClients
+            });
+        });
+        /*
+        for (var i = 0; i <= config.clients.length; i++) {
+            if (i === config.clients.length) {
+                res.render('settings', {
+                    title: 'Express',
+                    users: users,
+                    clients: clients
+                });
+            } else {
+                clients[i].userExists = false;
+                for (var j = 0; j < rawUsers.length; j++) {
+                    if (!clients[i].userExists) {
+                        if (rawUsers[j].clientId === config.clients[i].credentials.client_id) {
+                            clients[i].user = rawUsers[j];
+                            clients[i].userExists = true;
+                            fhir.request(rawUsers[j].clientId, rawUsers[j].accessToken, '/Patient').then(function(val) {
+                                var json = JSON.parse(val);
+
+                                console.log(JSON.stringify(json.entry));
+
+                                clients[i].user.info = json.entry;
+                            }).catch(function(err) {
+                                clients[i].user.info = {};
+                            });
+                        }
+                    }
+                }
+            }
+        }
+        */
+    }).catch(function (err) {
+        //console.log("Read users fails", err);
+        res.render('error', {
+            message: err,
+            error: err
+        });
+    });
 });
 
 /**
