82372a1224d68859d7ad0b5100b320fe2cc95baf
OEHokie <matt.martz@gmail.com>
2015-06-27 14:15:24 -0400

    UI and Client in User UI mostly the same as before.  Client info stored in User object

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('angularPassportApp')
-    .controller('SettingsCtrl', function ($scope) {
+    .controller('SettingsCtrl', function ($scope, Auth, $location, $rootScope) {
 
     });
