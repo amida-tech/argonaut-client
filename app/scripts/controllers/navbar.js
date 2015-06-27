227af22f1171e30eef73baaa96870c950d7442e2
OEHokie <matt.martz@gmail.com>
2015-06-27 13:26:28 -0400

    Home Page Working Home Page works with login/logout, user registration

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -2,20 +2,17 @@
 
 angular.module('angularPassportApp')
   .controller('NavbarCtrl', function ($scope, Auth, $location) {
-    $scope.menu = [{
-      "title": "Blogs",
-      "link": "blogs"
-    }];
+    $scope.menu = [];
 
     $scope.authMenu = [{
-      "title": "Create New Blog",
-      "link": "blogs/create"
+      "title": "Settings",
+      "link": "settings"
     }];
 
     $scope.logout = function() {
       Auth.logout(function(err) {
         if(!err) {
-          $location.path('/login');
+          $location.path('/');
         }
       });
     };
---
+++ app/scripts/controllers/settings.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('angularPassportApp')
+    .controller('SettingsCtrl', function ($scope) {
+
+    });
