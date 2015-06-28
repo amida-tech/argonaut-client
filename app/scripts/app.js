e5d5b00109492eae22eeae9269c0be380ae5330f
OEHokie <matt.martz@gmail.com>
2015-06-27 20:47:00 -0400

    mostly done UI stuff is next

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,23 +16,7 @@ angular.module('angularPassportApp', [
             })
             .when('/settings', {
                 templateUrl: 'partials/settings.html',
-                controller: 'SettingsCtrl'
-            })
-            .when('/blogs', {
-                templateUrl: 'partials/blogs/list.html',
-                controller: 'BlogsCtrl'
-            })
-            .when('/blogs/create', {
-                templateUrl: 'partials/blogs/create.html',
-                controller: 'BlogsCtrl'
-            })
-            .when('/blogs/:blogId/edit', {
-                templateUrl: 'partials/blogs/edit.html',
-                controller: 'BlogsCtrl'
-            })
-            .when('/blogs/:blogId', {
-                templateUrl: 'partials/blogs/view.html',
-                controller: 'BlogsCtrl'
+                controller: 'MainCtrl'
             })
             .when('/signup', {
                 templateUrl: 'partials/signup.html',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,27 +1,47 @@
 'use strict';
 
 angular.module('angularPassportApp')
-  .controller('MainCtrl', function ($scope, Auth, $location, $rootScope) {
-      $scope.error = {};
-      $scope.user = {};
+    .controller('MainCtrl', function ($scope, Auth, $location, $rootScope) {
+        $scope.error = {};
+        $scope.user = {};
 
-      $scope.login = function (form) {
-        Auth.login('password', {
-              'email': $scope.user.email,
-              'password': $scope.user.password
-            },
-            function (err) {
-              $scope.errors = {};
+        $scope.login = function (form) {
+            Auth.login('password', {
+                    'email': $scope.user.email,
+                    'password': $scope.user.password
+                },
+                function (err) {
+                    $scope.errors = {};
 
-              if (!err) {
-                $location.path('/');
-              } else {
-                angular.forEach(err.errors, function (error, field) {
-                  form[field].$setValidity('mongoose', false);
-                  $scope.errors[field] = error.type;
+                    if (!err) {
+                        $location.path('/');
+                    } else {
+                        angular.forEach(err.errors, function (error, field) {
+                            form[field].$setValidity('mongoose', false);
+                            $scope.errors[field] = error.type;
+                        });
+                        $scope.error.other = err.message;
+                    }
                 });
-                $scope.error.other = err.message;
-              }
-            });
-      };
-  });
+        };
+        for (var i = 0; i < $rootScope.currentUser.clients.length; i++) {
+            if ($rootScope.currentUser.clients[i].shortname === 'DRE') {
+                $rootScope.currentUser.clients[i].token = $rootScope.currentUser.dreToken;
+                if ($rootScope.currentUser.clients[i].token) {
+                    Auth.getDREUserInfo($rootScope.currentUser._id, $rootScope.currentUser.clients[i].credentials.client_id);
+                    Auth.getDREMeds($rootScope.currentUser._id, $rootScope.currentUser.clients[i].credentials.client_id);
+                }
+            }
+            if ($rootScope.currentUser.clients[i].shortname === 'SMART on FHIR') {
+                $rootScope.currentUser.clients[i].token = $rootScope.currentUser.smartToken;
+                if ($rootScope.currentUser.clients[i].token) {
+                    Auth.getSMARTUserInfo($rootScope.currentUser._id, $rootScope.currentUser.clients[i].credentials.client_id);
+                    Auth.getSMARTMeds($rootScope.currentUser._id, $rootScope.currentUser.clients[i].credentials.client_id);
+                }
+            }
+        }
+
+        $scope.revokeToken = function () {
+            Auth.revokeToken();
+        }
+    });
